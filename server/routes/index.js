6f1890b8c844aa9e536f0e436ff40658afd20e08
boomiin <akachris225@gmail.com>
2022-03-14 19:00:44 +0100

    login done

--- server/routes/index.js
+++ server/routes/index.js
@@ -17,7 +17,7 @@ router.get('/', (req, res, next) => {
     console.log('index');
 });
 
-// ajout d'utilisateur
+// inscription d'un utilisateur
 router.post('/addUser', validateRegister, async (req, res, next) => {
     var email = req.body.email;
     var pass = await hash(req.body.password);
@@ -39,12 +39,49 @@ router.post('/addUser', validateRegister, async (req, res, next) => {
         });
 });
 
-
+// connexion utilisateur
 router.post("/login", async function (req, res, next) {
     login(req.body)
         .then(user => user ? res.json(user) : res.status(400).json({ message: 'Les identifiants sont incorrects' }))
         .catch(err => next(err));
 });
 
+// ajout d'un objet
+router.post("/addItem", function (req, res, next) {
+    const nom = req.body.nom;
+    const description = req.body.description;
+
+    const item = Item({
+        nom: nom,
+        description: description
+    })
+
+    item
+        .save()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message:
+                    err.message || "Erreur de création de l'objet",
+            });
+        });
+});
+
+
+// affichage d'objets
+router.get("/listItems", function (req, res, next) {
+    Item.find()
+        .then((data) => {
+            res.send(data)
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message:
+                    err.message || "Erreur lors de la recupération des objets",
+            });
+        })
+});
 
 module.exports = router;
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -3,10 +3,13 @@ const jwt = require('jsonwebtoken');
 const config = require('../config.json');
 const User = require("../models/User");
 
+// hash du mot de passe
 function hash(password) {
     return bcrypt.hash(password, 10);
 }
 
+// on recupère l'utilisateur inscrit par son  e-mail
+// son compare le mot de passe entré et celui d'origine, s'ils matchent on cré un token de connexion qu'on retourne au client
 async function login({ email, password }) {
     const user = await User.findOne({ email });
     if (user && bcrypt.compareSync(password, user.password)) {
